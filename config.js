8888e35ba306fd57b2088eae56cef4f5a65fc6f0
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2020-07-02 00:27:42 +0430

    Start to make it happen. Using HTML5 UP's Directive template

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: 'Mohammad Sadegh Shad',
+  manifestName: 'directive',
+  manifestShortName: 'Landing',
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`,
+  socialLinks: [{
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/m-sadegh-sh',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/@m_sadegh_sh',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/mohammad-sadegh-shad/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:m.sadegh.sh@gmail.com',
+    },
+  ],
+};
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div id="header">
+      <span className="logo icon fa-paper-plane-o"></span>
+      <h1>Hey! It's Mohammad Sadegh Shad</h1>
+      <div className="container medium">
+        <p>I'm an experienced Full-stack developer with more than 8 years of experience in developing and deploying corporate websites and web services for government bodies and commercial organizations with strong proficiency in the back-end (ASP.NET Core / C#) and front-end (HTML5 / CSS3 / React.js / Redux / Bootstrap) development.</p>
+        <br/>
+        <p>I'm also a rookie and enthusiastic in the field of Mobile application development using Java and Flutter.</p>
+      </div>
+    </div>
+  );
+}
